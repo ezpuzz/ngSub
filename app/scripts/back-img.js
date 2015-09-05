03289b0f59d1e5504ffdc92518e16079cd249aae
Emory Petermann <emory@onlyfor.us>
2015-09-04 19:05:10 -0700

    update dependencies and consolide now playing displays fix inconsistent album list heights clean up some bad bootstrap markup fix the progress bar height display and remove some extra clearfixes add coverflow switch to settings (not yet working)

---
+++ app/scripts/back-img.js
@@ -0,0 +1,9 @@
+ngSub.directive('backImg', function(){
+    return function(scope, element, attrs){
+        attrs.$observe('backImg', function(value) {
+            element.css({
+                'background-image': 'url(' + value +')',
+            });
+        });
+    };
+});
